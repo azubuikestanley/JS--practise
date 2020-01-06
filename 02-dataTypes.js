876296007ebf3b8e5a1b6462086d666e67d29a49
azubuikestanley <azubuikestanley@gmail.com>
2020-01-06 15:22:23 +0100

    JS practice first commit

---
+++ 02-dataTypes.js
@@ -0,0 +1,173 @@
+/* Data type
+ undefined, null, boolean, string, symbol, number and object */
+ console.log("Data types in javascript are 'undefined, null, boolean, string, symbol, number and object'");
+
+console.log("-----------------------------");
+
+var myName = "John"
+
+myName = 5
+
+let ourName = "coding"
+
+const pi = 3.14
+
+console.log(myName);
+
+//Storing Values with Assignment Operator
+var a;  //decearing a variable
+var b = 6; //variable b was decleared and assigned to 6
+console.log(a)
+console.log(b)
+
+a = 18; //variable a that was decleared on line 16 is now assigned a value of 8
+
+b = a; //content of variable a is assigned to b
+
+console.log(a)
+console.log("-----------------------------");
+
+
+
+
+//Initializing Variable w/ Assignment operator
+var a = 9; //var a declears the variable and = 9 initializes the variable
+
+//Uninitialized variables
+//Initiialize these three variables
+var a = 5;
+var b = 10;
+var c = "I am a ";
+
+//Do not change code below this line
+a = a + 1;
+b = b + 5;
+c = c + "String!";
+console.log(a, b, c);
+console.log("-----------------------------");
+
+
+
+//Case Sensitivity in variables  
+var StUdLyCapVaR;
+var properCamelCase;
+var TitleCaseOver;
+
+//Assignments (1st three are wrong and wont be assigned)
+StUDLYCAPVAR = 10;
+PRoperCAmelCAse = "A string";
+tiTLEcASEoVER = 9000;
+//It is proper to use camel case in declearing variable as shown below
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+//Assignments 
+studlyCapVar = 10;
+properCamelCase = "A string";
+titleCaseOver = 9000;
+console.log(studlyCapVar, properCamelCase, titleCaseOver);
+console.log("-----------------------------");
+
+
+
+//Adding Numbers
+var sum = 10 + 2;
+console.log(sum);
+
+//Substracting Numbers
+var difference = 50 - 2;
+console.log(difference);
+
+//Multiplying Numbers
+var product = 5 * 2;
+console.log(product);
+
+//Dividing Numbers
+var quotient = 66 / 33;
+console.log(quotient);
+
+//Incrementing Numbers
+var myVar = 66;
+//Only change code below this line
+myVar = myVar + 1 // myVar++ does same as this
+console.log(myVar);
+myVar++
+console.log(myVar);
+myVar += 1
+console.log(myVar);
+
+//Decrementing Numbers
+var myVar = 66;
+//Only change code below this line
+myVar = myVar - 1 // myVar++ does same as this
+console.log(myVar);
+myVar--
+console.log(myVar);
+myVar -= 1
+console.log(myVar);
+
+//Decimal Numbers
+var ourDecimal = 5.7;
+//Only change code below this line
+var myDecimal = 0.009
+
+//Multiplying decimals
+var product = 2.0 * 2.5;
+console.log(product);
+
+//Divide decimals
+var quotient = 4.4 / 2.0;
+console.log(quotient);
+
+//Finding a remainder
+var remainder
+remainder = 11 % 3;
+console.log(remainder);
+
+//Compound Assignment with augmented addition
+var a = 2;
+var b = 14;
+var c = 15;
+//only modify code below this ine
+a += 11;
+b += 9;
+c += 8;
+
+//Compound Assignment with augmented Subtraction
+var a = 2;
+var b = 14;
+var c = 15;
+//only modify code below this ine
+a -= 11;
+b -= 9;
+c -= 8;
+
+//Compound Assignment with augmented Multiplication
+var a = 2;
+var b = 14;
+var c = 15;
+//only modify code below this ine
+a *= 11;
+b *= 9;
+c *= 8;
+
+//Compound Assignment with augmented Division
+var a = 2;
+var b = 14;
+var c = 15;
+//only modify code below this ine
+a /= 11;
+b /= 9;
+c /= 8;
+console.log("-----------------------------");
+
+
+
+
+
+
+
+
+
+
+
---
+++ 03-stringVariable.js
@@ -0,0 +1,130 @@
+//Example -  string
+var firstName = "Rice";
+var lastName = "Doe";
+
+//Only change code below this line
+var myFirstName = 'Bean';
+var myLastName = `Bread`;
+console.log("---------------------");
+
+//Escaping Literal Quotes in Strings
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
+console.log(myStr);
+
+//Quoting Strings with single quotes
+var myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStr2);
+//Quoting Strings with sbackticks
+var myStr2 = `'<a href="http://www.example.com" target="_blank">Link</a>'`;
+console.log("---------------------");
+
+
+//Escape sequences in strings
+/*****
+CODE OUTPUT
+\' single quote
+\' double quote
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+*****/
+
+var myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
+console.log(myStr)
+
+console.log("---------------------");
+
+//Concatenating strings with plus operator
+// Example
+var ourStr = "I come first." + "I come second.";
+console.log(ourStr)
+console.log("---------------------");
+
+//Concatenating strings with plus Equals operator
+var ourStr = "This is the first sentence.";
+ourStr += "This is the second sentence.";
+console.log(ourStr)
+console.log("---------------------");
+
+//Constructing strings with variables
+// Example
+var ourName = "CodeCamp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+console.log(ourStr)
+console.log("---------------------");
+
+//Appending  variables to strings
+// Example
+var anAdjective = "awesome";
+var ourStr = "CodeCamp is ";
+ourStr += anAdjective;
+console.log(ourStr)
+console.log("---------------------");
+
+//Finding length of string
+//Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName.length;
+console.log(firstNameLength)
+console.log("---------------------");
+
+//Bracket Notation to find First Character in string
+//Example
+var firstNameLength = 0;
+var firstName = "Ada";
+
+firstNameLength = firstName[0];
+console.log(firstNameLength)
+console.log("---------------------");
+
+//String Immutability
+//Example
+var myStr = "Aello World";
+
+myStr = "Hello World";
+console.log(myStr)
+console.log("---------------------");
+
+//Bracket Notation to find Nth Character in string
+//Example
+var firstName = "Ada";
+var secondLetterOfFirstName = firstName[1];
+
+console.log(secondLetterOfFirstName)
+console.log("---------------------");
+
+//Bracket Notation to find last Character in string
+//Example
+var firstName = "Ada";
+var lastLetterOfFirstName = firstName[firstName.length - 1];
+
+console.log(lastLetterOfFirstName)
+console.log("---------------------");
+
+//Bracket Notation to find Nth-to-last Character in string
+//Example
+var firstName = "Ada";
+var thirdToLetterOfFirstName = firstName[firstName.length - 3];
+
+console.log(thirdToLetterOfFirstName)
+console.log("---------------------");
+
+
+//Word Blanks
+//Example
+function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    //Your code below this line
+    var result = "";
+
+    //Your code above this line
+    return result;
+}
+//Change the words here to test your function
+
+console.log(wordBlanks("dog", "big", "ran", "quickly",));
+console.log("---------------------");
